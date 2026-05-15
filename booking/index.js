66baa268a42b30c71eb66ddad3ed77013dcf11d8
meriam gahbiche <meriam.gahbiche@polytechnicien.tn>
2026-05-15 12:53:06 +0100

    commit 3

--- booking/index.js
+++ booking/index.js
@@ -1,7 +1,7 @@
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const path = require('path');
-const BookingService = require('./service');
+const BookingService = require('../service');
 const kafkaProducer = require('./kafka/producer');
 
 const PROTO_PATH = path.join(__dirname, 'proto', 'booking.proto');
